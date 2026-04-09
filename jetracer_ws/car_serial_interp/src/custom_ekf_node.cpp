a6102e2424b02c266c3d42db44b142e01b6863a9
Aleksander Røder <fylling8@gmail.com>
2026-04-09 10:55:36 +0200

    Changed from base_footprint to base_link

--- jetracer_ws/car_serial_interp/src/custom_ekf_node.cpp
+++ jetracer_ws/car_serial_interp/src/custom_ekf_node.cpp
@@ -8,6 +8,7 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Matrix3x3.h>
 #include <Eigen/Dense>
 #include <cmath>
 #include <memory>
